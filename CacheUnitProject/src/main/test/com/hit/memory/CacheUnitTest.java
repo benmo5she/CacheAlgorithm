e405975ea12a4b364c2ebc803c8b669e1eb6f05b
Ben Moshe <benmo5she@gmail.com>
2018-04-22 02:22:32 +0300

    Updated test

--- CacheUnitProject/src/main/test/com/hit/memory/CacheUnitTest.java
+++ CacheUnitProject/src/main/test/com/hit/memory/CacheUnitTest.java
@@ -14,30 +14,7 @@ import com.hit.dm.DataModel;
 public class CacheUnitTest {
 	@Test
 	public void testLRUPut() {
-
-		/*
-		 * IDao<Long,DataModel<Integer>> dao = new
-		 * DaoFileImpl<>("C:\\study\\javaTest.txt"); dao.save(new
-		 * DataModel<Integer>((long)1,2));
-		 * System.out.println(dao.find((long)1).getContent());
-		 */
-
 		File pagesStorage = new File("C:\\study\\javaTest.txt");
-		// If file does not exist it will create it,otherwise will just use the existing
-		// one
-/*		try {
-			pagesStorage.createNewFile();
-			HashMap<Long, DataModel<Integer>> map = new HashMap<>();
-			map.put((long) 1, new DataModel<Integer>((long) 1, 2));
-			FileOutputStream os = new FileOutputStream(pagesStorage, false);
-			ObjectOutputStream oos = new ObjectOutputStream(os);
-			oos.writeObject(map);
-			oos.close();
-			os.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
 		try {
 			pagesStorage.createNewFile();
 		} catch (IOException e1) {
@@ -45,7 +22,7 @@ public class CacheUnitTest {
 			e1.printStackTrace();
 		}
 		IAlgoCache<Long, DataModel<Integer>> algo = new LRUAlgoCacheImpl<Long, DataModel<Integer>>(3);
-		IDao<Long, DataModel<Integer>> dao = new DaoFileImpl<>("C:\\study\\javaTest.txt");
+		IDao<Long, DataModel<Integer>> dao = new DaoFileImpl<>(pagesStorage.getAbsolutePath());
 		CacheUnit<Integer> testCU = new CacheUnit<Integer>(algo, dao);
 		dao.save(new DataModel<Integer>((long) 1, 2));
 		DataModel<Integer>[] resultArray = null;
